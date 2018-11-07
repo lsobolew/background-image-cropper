b51b85192319ae0f1c79a5fedc277936779e76a6
Łukasz Sobolewski <lukasz.sobolewski@getresponse.com>
2018-11-07 21:21:00 +0100

    back to drawing board + static images instead of dummy img

--- main.js
+++ main.js
@@ -11,42 +11,17 @@ function calculateBg(el) {
     var imgWidth = el.getAttribute('data-bg-image-width');
     var imgHeight = el.getAttribute('data-bg-image-height');
     var heightRatio, widthRatio;
-    var resize = '0x0';
+    var resize = bg.width + 'x' + bg.height;
     var img = {
-        width: null,
-        height: null
+        width: imgWidth,
+        height: imgHeight
     }
-
-    if(bgImg.width === 'auto' && bgImg.height !== 'auto') {
-        widthRatio = heightRatio = (bgImg.height / imgHeight);
-        resize = 'x' + Math.min(imgHeight, bgImg.height);
-    } else if(bgImg.height === 'auto' && bgImg.width !== 'auto') {
-        heightRatio = widthRatio = (bgImg.width / imgWidth);
-        resize = Math.min(imgWidth, bgImg.width) + 'x';
-    } else {
-        widthRatio = 1;
-        heightRatio = 1;
-    }
-// console.log(bg.width / bg.height, imgWidth / imgHeight);
-    if((bg.width / bg.height) < (imgWidth / imgHeight)) {
-        img.width = (bg.width + (bg.width - bg.containWidth)) / widthRatio;
-        img.height = bg.height / heightRatio;
-    } else if ((bg.width / bg.height) > (imgWidth / imgHeight)){
-        img.width = bg.width / widthRatio;
-        img.height = (bg.height + (bg.height - bg.containHeight)) / heightRatio;
-    } else {
-        img.width = bg.width / widthRatio;
-        img.height = bg.height / heightRatio;
-    }
-
-    // img.width = img.width - bgPositionX / widthRatio;
-    // img.height = img.height - bgPositionY / heightRatio;
 
     img.width = Math.round(img.width);
     img.height = Math.round(img.height);
 
     // el.style.backgroundImage = `url(http://localhost:8090/cw${bg.width + bg.offsetLeft - 3},ch${bg.height + bg.offsetTop - 3}/https://dummyimage.com/${imgWidth}x${imgHeight}/000/fff)`;
-    el.style.backgroundImage = `url(http://localhost:8090/${resize},cw${img.width},ch${img.height},q60/https://dummyimage.com/${imgWidth}x${imgHeight}/000/fff)`;
+    el.style.backgroundImage = `url(http://localhost:8090/${resize},fit,cw${img.width},ch${img.height},q100/${getComputedStyle(el).backgroundImage.split('"')[1]}`;
 }
 
 
@@ -80,26 +55,22 @@ function getBackgroundRectSize(el) {
             bg.width = el.offsetWidth;
             bg.height = el.offsetHeight;
         } else if(elementProperties.backgroundOrigin === "padding-box") {
-            bg.width = el.offsetWidth - elementProperties.borderLeftWidth;
-            bg.height = el.offsetHeight - elementProperties.borderTopWidth;
-            bg.containWidth = el.offsetWidth - elementProperties.borderLeftWidth - elementProperties.borderRightWidth;
-            bg.containHeight = el.offsetHeight - elementProperties.borderTopWidth - elementProperties.borderBottomWidth;
+            bg.width = el.offsetWidth - elementProperties.borderRightWidth - elementProperties.borderLeftWidth;
+            bg.height = el.offsetHeight - elementProperties.borderBottomWidth - elementProperties.borderTopWidth;
         } else if(elementProperties.backgroundOrigin === "content-box") {
-            bg.width = el.offsetWidth - elementProperties.borderLeftWidth - elementProperties.paddingLeft;
-            bg.height = el.offsetHeight - elementProperties.borderTopWidth - elementProperties.paddingTop;
+            bg.width = el.offsetWidth - elementProperties.borderRightWidth - elementProperties.borderLeftWidth - elementProperties.paddingRight - elementProperties.paddingLeft;
+            bg.height = el.offsetHeight - elementProperties.borderBottomWidth - elementProperties.borderTopWidth - elementProperties.paddingBottom - elementProperties.paddingTop;
         }
     } else if(elementProperties.backgroundAttachment === "local"){
         if(elementProperties.backgroundOrigin === "border-box") {
-            bg.offsetLeft = elementProperties.borderLeftWidth;
-            bg.offsetTop = elementProperties.borderTopWidth;
-            bg.width = el.scrollWidth + bg.offsetLeft;
-            bg.height = el.scrollHeight + bg.offsetTop;
+            bg.width = el.scrollWidth + elementProperties.borderLeftWidth + elementProperties.borderRightWidth;
+            bg.height = el.scrollHeight + elementProperties.borderTopWidth + elementProperties.borderBottomWidth;
         } else if(elementProperties.backgroundOrigin === "padding-box") {
             bg.width = el.scrollWidth;
             bg.height = el.scrollHeight;
         } else if(elementProperties.backgroundOrigin === "content-box") {
-            bg.width = el.scrollWidth - elementProperties.paddingLeft;
-            bg.height = el.scrollHeight - elementProperties.paddingTop;
+            bg.width = el.scrollWidth - elementProperties.paddingLeft - elementProperties.paddingRight;
+            bg.height = el.scrollHeight - elementProperties.paddingTop - elementProperties.paddingBottom;
         }
     }
     el.bgSize = bg;
@@ -134,11 +105,24 @@ function getBackgroundImageSize(el) {
             height = backgroundRectSize.height;
         }
     } else if(sizes.length === 1){
-        width = parseFloat(sizes[0], 10);
+        if(sizes[0].indexOf('%') === -1) {
+            width = parseFloat(sizes[0], 10);
+        } else {
+            width = (parseFloat(sizes[0], 10) * backgroundRectSize.width) / 100;
+        }
         height = 'auto';
     } else {
-        width = parseFloat(sizes[0], 10);
-        height = parseFloat(sizes[1], 10);
+        if(sizes[0].indexOf('%') === -1) {
+            width = parseFloat(sizes[0], 10);
+        } else {
+            width = (parseFloat(sizes[0], 10) * backgroundRectSize.width) / 100;
+        }
+
+        if(sizes[0].indexOf('%') === -1) {
+            height = parseFloat(sizes[0], 10);
+        } else {
+            height = (parseFloat(sizes[1], 10) * backgroundRectSize.height) / 100;
+        }
     }
     el.bgImgSize = {width: width, height: height};
     return {width, height};
